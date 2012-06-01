83962d9dbb13bcc8c476589560d20a085884db26
savin <mafei.198@gmail.com>
2012-06-01 17:04:57 +0800

    update

--- lib/concurrent.rb
+++
@@ -1,5 +0,0 @@
-root = File.expand_path(File.dirname(__FILE__))
-
-require File.join(root, 'concurrent', 'client')
-require File.join(root, 'concurrent', 'ticket')
-require File.join(root, 'concurrent', 'redis_key')
